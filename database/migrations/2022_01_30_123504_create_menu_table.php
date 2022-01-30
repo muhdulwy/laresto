27771636da1ca6fae4ff29562f7694ac4e87aca2
ulwy <ulwy.rpl@gmail.com>
2022-01-30 20:02:10 +0700

    migration, menu model, menucontroller

---
+++ database/migrations/2022_01_30_123504_create_menu_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMenuTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('menu', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama', 45);
+            $table->string('deskripsi');
+            $table->enum('kategori', ['makanan', 'minuman']);
+            $table->float('harga');
+            $table->string('foto',45);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('menu');
+    }
+}
---
+++ database/migrations/2022_01_30_123536_create_orders_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id(); 
+            $table->foreignId('menu_id')->references('id')->on('menu');
+            $table->string('jumlah', 45);
+            $table->date('tgl_order');
+           
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
